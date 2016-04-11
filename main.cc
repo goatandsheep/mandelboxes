a890be3f9692193f7d3c9283e3b4ea4f2178d3a9
Erik Lacharite <lacharef@mcmaster.ca>
2016-04-10 22:59:12 -0400

    added required behavior

--- main.cc
+++ main.cc
@@ -47,7 +47,7 @@ extern void getParameters(char *filename, CameraParams *camera_params, RenderPar
 extern void init3D       (CameraParams *camera_params, const RenderParams *renderer_params);
 extern void renderFractal(const CameraParams &camera_params, const RenderParams &renderer_params, unsigned char* image);
 extern void saveBMP(const char* filename, const unsigned char* image, int width, int height);
-extern void fillDefaultParams(char *filename, CameraParams *camP, RenderParams *renP, MandelBoxParams *boxP);
+extern void fillDefaultParams(CameraParams *camP, RenderParams *renP, MandelBoxParams *boxP);
 // extern void print_renderer(RenderParams *renP);
 // extern void print_camera(CameraParams *camP);
 extern void generateCameraPath(CameraParams &camera_params, RenderParams &renderer_params, CameraParams *camera_params_array, int frames, double camera_speed);
@@ -59,7 +59,7 @@ extern double getTime();
 
 MandelBoxParams mandelBox_params;
 struct stat st = {0};
-#define PI 3.14159265
+// #define PI 3.14159265 // used for camera 360
 
 int main(int argc, char** argv)
 {
@@ -71,38 +71,21 @@ int main(int argc, char** argv)
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     char processor_name[MPI_MAX_PROCESSOR_NAME];
     MPI_Get_processor_name(processor_name, &namelen);
-    printf("Nodes (tot): %d, Name (Host): %s, Rank: %d\n",size,processor_name,rank);
+    printf("Nodes: %d, Name: %s, Rank: %d\n",size,processor_name,rank);
   #else
     int rank = 0;
     int size = 1;
   #endif
 
 
+
   #if defined(_OPENMP)
-    //int nProcessors=omp_get_max_threads();
+    //int nProcessors=omp_get_max_threads(); // not used due to the need for an external variable to be set
     int nProcessors=omp_get_num_procs();
     omp_set_num_threads(nProcessors);
   #endif
 
   // begin normal program
-  CameraParams    camera_params;
-  RenderParams    renderer_params;
-  fillDefaultParams(argv[1], &camera_params, &renderer_params, &mandelBox_params); // this will fill with data from getparams.c
-
-  // Modify Default Params
-  camera_params.camTarget[0] = 7;
-  camera_params.camTarget[1] = 2;
-  camera_params.camTarget[2] = 0;
-  camera_params.camPos[0] = 10;
-  camera_params.camPos[1] = 0;
-  camera_params.camPos[2] = 0;
-
-  // Output Video Params
-  int frames = 400;
-  int out_width = 100; //1920
-  int out_height = 100; //1080
-  double camera_speed = 0.05;
-
 
   // make sure output directory exists
   if (stat("_images", &st) == -1)
@@ -114,6 +97,94 @@ int main(int argc, char** argv)
     mkdir("_videos", 0700);
   }
 
+  CameraParams    camera_params;
+  RenderParams    renderer_params;
+
+  if (argc > 1){ // original behavior
+    getParameters(argv[1], &camera_params, &renderer_params, &mandelBox_params);
+    int image_size = renderer_params.width * renderer_params.height;
+    unsigned char *image = (unsigned char*)malloc(3*image_size*sizeof(unsigned char));
+    init3D(&camera_params, &renderer_params);
+    renderFractal(camera_params, renderer_params, image);
+    saveBMP(renderer_params.file_name, image, renderer_params.width, renderer_params.height);
+    free(image);
+  }
+  else{ // do auto path (run with no arguments just "./mandelbox")
+
+  fillDefaultParams(&camera_params, &renderer_params, &mandelBox_params); // this will fill with data from getparams.c
+  // getParameters(argv[1], &camera_params, &renderer_params, &mandelBox_params); can load defaults from file
+
+  // Modify Default Params (starting params)
+  camera_params.camPos[0] = 20;
+  camera_params.camPos[1] = 20;
+  camera_params.camPos[2] = 7;
+
+  camera_params.camTarget[0] = -4;
+  camera_params.camTarget[1] = -4;
+  camera_params.camTarget[2] = -1;
+
+  double fov = 1; //need to re-set this as the pathing defaults to 0.4
+
+
+  // Output Video Params
+  int frames = 10000;
+  int out_width = 1920; //1920
+  int out_height = 1080; //1080
+  double camera_speed = 0.01; //0.01 for 30 fps
+
+
+  // Generate Path locally and then distribute to MPI cluster for HD rendering
+  CameraParams *camera_params_array = (CameraParams *)malloc(sizeof(CameraParams)*frames);//forward (0), left (45), right (45), top (45), bottom (45)
+  if (rank ==0) generateCameraPath(camera_params, renderer_params, camera_params_array, frames,camera_speed);
+
+  #ifdef USE_MPI
+    if (size >1){
+    MPI_Bcast(camera_params_array, sizeof(CameraParams)*frames,MPI_CHAR, 0, MPI_COMM_WORLD);
+    if (rank==0)printf("Broadcasting Path\n");
+    }
+  #endif
+
+  // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! BEGIN HD MPI !!!!!!!!!!!!!!!!!!!!!!!!!
+  if (rank==0 and size == 1) printf("Rendering HD Images\n");
+  if (rank==0 and size > 1) printf("Rendering HD Images Across %d Nodes\n",size);
+  renderer_params.width = out_width;
+  renderer_params.height = out_height;
+
+  int image_size = renderer_params.width * renderer_params.height;
+  unsigned char *image = (unsigned char*)malloc(3*image_size*sizeof(unsigned char));
+  init3D(&camera_params, &renderer_params);
+
+
+  camera_params.fov = fov;
+  double time;
+  if (rank == 0) time = getTime(); // was before pragma loop
+
+  int i;
+  for (i=rank; i<frames;i+=size){
+    snprintf(renderer_params.file_name,80,"_images/image%010d.bmp",i+1);
+    camera_params= camera_params_array[i];
+    camera_params.fov = fov;
+
+    init3D(&camera_params, &renderer_params);
+    renderFractal(camera_params, renderer_params, image);
+
+
+    if (rank==0) printProgress((i+1)/(double)frames,getTime()-time);
+    saveBMP(renderer_params.file_name, image, renderer_params.width, renderer_params.height);
+  }
+  if (rank==0) printProgress((double)1,getTime()-time); //make it end at 100%
+
+  free(image);
+  }
+
+
+
+  if (rank==0) printf("\n");
+  #ifdef USE_MPI
+   MPI_Finalize();
+  #endif
+  return 0;
+}
 
 
 
@@ -135,53 +206,3 @@ int main(int argc, char** argv)
   // }
   // free(image);
 
-
-  // Generate Path locally and then distribute to MPI cluster for HD rendering
-  CameraParams *camera_params_array = (CameraParams *)malloc(sizeof(CameraParams)*frames);//forward (0), left (45), right (45), top (45), bottom (45)
-  if (rank ==0) generateCameraPath(camera_params, renderer_params, camera_params_array, frames,camera_speed);
-
-  #ifdef USE_MPI
-    if (size >1){
-    MPI_Bcast(camera_params_array, sizeof(CameraParams)*frames,MPI_CHAR, 0, MPI_COMM_WORLD);
-    printf("Broadcasting Path\n");
-    }
-  #endif
-
-  // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! BEGIN HD MPI !!!!!!!!!!!!!!!!!!!!!!!!!
-  printf("Rendering HD Images\n");
-  renderer_params.width = out_width;
-  renderer_params.height = out_height;
-
-  int image_size = renderer_params.width * renderer_params.height;
-  unsigned char *image = (unsigned char*)malloc(3*image_size*sizeof(unsigned char));
-  init3D(&camera_params, &renderer_params);
-
-
-  camera_params.fov = 1;
-  double time;
-  if (rank == 0) time = getTime(); // was before pragma loop
-
-  int i;
-  for (i=rank; i<frames;i+=size){
-    snprintf(renderer_params.file_name,80,"_images/image%010d.bmp",i+1);
-    camera_params= camera_params_array[i];
-    camera_params.fov = 1;
-    init3D(&camera_params, &renderer_params);
-
-    renderFractal(camera_params, renderer_params, image);
-
-
-    if (rank==0) printProgress((i+1)/(double)frames,getTime()-time);
-    saveBMP(renderer_params.file_name, image, renderer_params.width, renderer_params.height);
-  }
-  if (rank==0) printProgress((double)1,getTime()-time);
-
-  free(image);
-
-
-  if (rank==0) printf("\n");
-  #ifdef USE_MPI
-   MPI_Finalize();
-  #endif
-  return 0;
-}
